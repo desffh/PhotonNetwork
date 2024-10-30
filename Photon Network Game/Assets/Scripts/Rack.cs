aa71560ad9999b5bb8a2b35fb6a4af6d1bbc3f9d
chichi <yangju058@gmail.com>
2024-10-30 19:45:19 +0900

    (2024.10.30) 상태 패턴

--- Photon Network Game/Assets/Scripts/Rack.cs
+++ Photon Network Game/Assets/Scripts/Rack.cs
@@ -1,29 +1,79 @@
+using Photon.Pun;
+using Photon.Pun.Demo.SlotRacer.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum State
+{
+    WALK,
+    ATTACK,
+    DIE
+}
+
 public class Rack : MonoBehaviour
 {
+    [SerializeField] State state;
+    [SerializeField] Animator animator;
+
     [SerializeField] GameObject destination;
 
     [SerializeField] NavMeshAgent navMeshAgent;
 
+
     // 컴포넌트 가져오는건 Awake에서 하기
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     void Start()
     {
-
+        state = State.WALK;
         destination = GameObject.Find("Projector Star");
     }
 
     
     void Update()
     {
-        navMeshAgent.SetDestination(destination.transform.position);
+    
+        switch(state)
+        {
+            case State.WALK: Walk();
+                break;
+
+            case State.ATTACK: Attack(); 
+                break;
+
+            case State.DIE: Die(); 
+                break;
+        }
     }
+
+    public void Walk()
+    {
+        navMeshAgent.SetDestination(destination.transform.position);
+
+    }
+
+    public void Attack()
+    {
+        animator.Play("Attack");
+    }
+
+    public void Die()
+    {
+        animator.SetTrigger("Die");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Projector Star"))
+        {
+            state = State.ATTACK;
+        }
+    }
+
 }
